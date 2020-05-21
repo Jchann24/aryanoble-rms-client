73e07f159ca981847ace2a5b89b0bc3b3b526f1f
Jason  Chan <jasonnchann24@gmail.com>
2020-05-21 07:00:17 +0700

    error sidenav

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 export default {
   mode: 'universal',
   /*
@@ -14,7 +16,46 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'sytlesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'assets/vendor/nucleo/css/nucleo.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'assets/vendor/@fortawesome/fontawesome-free/css/all.min.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'assets/css/argon.css?v=1.2.0',
+        type: 'text/css'
+      }
+    ],
+    script: [
+      { src: 'assets/vendor/jquery/dist/jquery.min.js', body: true },
+      {
+        src: 'assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js',
+        body: true
+      },
+      { src: 'assets/vendor/js-cookie/js.cookie.js', body: true },
+      {
+        src: 'assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js',
+        body: true
+      },
+      {
+        src: 'assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js',
+        body: true
+      },
+      { src: 'assets/js/argon.js?v=1.2.0', body: true }
+    ]
   },
   /*
    ** Customize the progress-bar color
@@ -48,14 +89,17 @@ export default {
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
-  axios: {},
+  axios: {
+    baseUrl: process.env.API_URL
+  },
   /*
    ** Build configuration
    */
   build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {}
+    extend(config, { isClient }) {
+      if (isClient) {
+        config.devtool = 'source-map'
+      }
+    }
   }
 }
