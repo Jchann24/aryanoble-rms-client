4e72328657ce10a8b471c364108cc6d6ec3c2e48
Jason  Chan <jasonnchann24@gmail.com>
2020-07-02 15:00:49 +0700

    Quote component

---
+++ components/core/Attributes/Quote.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card bg-dark text-white border-0">
+    <img
+      class="card-img"
+      src="https://source.unsplash.com/collection/460289/1600x900/?orientation=landscape"
+      loading="lazy"
+      alt="Quote Image"
+      style="max-height: 200px; min-height: 200px; object-fit: cover; filter:brightness(40%);"
+      crossorigin="anonymous"
+    />
+    <div class="card-img-overlay d-flex align-items-center px-xl-4">
+      <div>
+        <h5 class="h2 card-title text-white mb-2">Quote of the Day</h5>
+        <div v-if="quote">
+          <p class="card-text">
+            {{ quote.content }}
+          </p>
+          <p class="card-text text-sm font-weight-bold">
+            {{ quote.author }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quote',
+  data() {
+    return {
+      quote: null
+    }
+  },
+  created() {
+    this.getQuote()
+  },
+  methods: {
+    async getQuote() {
+      const res = await this.$axios.$get('https://api.quotable.io/random')
+      this.quote = res
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/div/index.vue
+++ pages/div/index.vue
@@ -67,29 +67,7 @@
     <div class="container-fluid mt--6">
       <div class="row">
         <div class="col-12 col-md-8">
-          <div class="card bg-dark text-white border-0">
-            <img
-              class="card-img"
-              src="https://source.unsplash.com/collection/460289/1600x900/?orientation=landscape"
-              loading="lazy"
-              alt="Quote Image"
-              style="max-height: 200px; min-height: 200px; object-fit: cover; filter:brightness(40%);"
-              crossorigin="anonymous"
-            />
-            <div class="card-img-overlay d-flex align-items-center px-xl-4">
-              <div>
-                <h5 class="h2 card-title text-white mb-2">Quote of the Day</h5>
-                <div v-if="quote">
-                  <p class="card-text">
-                    {{ quote.content }}
-                  </p>
-                  <p class="card-text text-sm font-weight-bold">
-                    {{ quote.author }}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <quote />
         </div>
         <div class="col-12 col-md-4 align-self-center">
           <div class="card bg-gradient-primary">
@@ -243,18 +221,15 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import Quote from '@/components/core/Attributes/Quote'
 import Footer from '@/components/core/Footer.vue'
 
 export default {
   middleware: ['auth', 'div'],
   name: 'DivUserDashboard',
   components: {
+    Quote,
     Footer
-  },
-  data() {
-    return {
-      quote: null
-    }
   },
   computed: {
     ...mapGetters({
@@ -265,18 +240,12 @@ export default {
   created() {
     this.getERFS()
     this.getCARDS()
-    this.getQuote()
   },
   methods: {
     ...mapActions({
       getERFS: 'erfs/GET_ERFS',
       getCARDS: 'candidate-cards/GET_CANDIDATE_CARDS'
-    }),
-
-    async getQuote() {
-      const res = await this.$axios.$get('https://api.quotable.io/random')
-      this.quote = res
-    }
+    })
   }
 }
 </script>
